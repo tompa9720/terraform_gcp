0bae8af8163163aca0aa16b9478ef3b6ebc3fe4e
tompa9720 <tompawilliams@gmail.com>
2025-02-18 23:40:45 +0000

    modified workflow

--- infra/main.tf
+++ infra/main.tf
@@ -3,3 +3,9 @@ resource "google_storage_bucket" "terraform_state" {
   location = "EU"
   storage_class = "STANDARD"
 }
+
+resource "google_storage_bucket" "test_workflow" {
+  name     = "test"
+  location = "EU"
+  storage_class = "STANDARD"
+}
